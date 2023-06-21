c741c134b7e310ca4309ecbdd459aa982a372d4c
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2023-06-21 08:44:14 -0300

    Passando parâmetros para o serviço RESTfull.

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -4,15 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController//Arquitetura REST
 @RequestMapping(value = "/usuario")
 public class IndexController {
 
+	//Serviço RESTFul
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Olá Usuário REST Spring Boot", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value="nome", required = true, defaultValue = "?") String nome, @RequestParam (value="salario", required = true, defaultValue = "0") Long Salario) {
+		return new ResponseEntity("Olá Usuário REST Spring Boot " + nome +" salario " + Salario, HttpStatus.OK);
 	}
 	
 
