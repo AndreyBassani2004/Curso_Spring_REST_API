03c8a6519bee1b0c49ab63daeda343c2e004e48f
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2023-06-25 19:37:06 -0300

    Cross Origin - Configuração centralizada.

--- cursospringrestapi/src/main/java/curso/api/rest/ServletInitializer.java
+++ cursospringrestapi/src/main/java/curso/api/rest/ServletInitializer.java
@@ -2,13 +2,27 @@ package curso.api.rest;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer implements WebMvcConfigurer{
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CursospringrestapiApplication.class);
 	}
 
+	
+	//Mapeamento Global Reflete em todo o sistema
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/usuario/**")
+		.allowedMethods("*");
+		//.allowedOrigins("https//www.cliente40.com.br","https//www.cliente60.com.br");
+		//Liberando requisições para cliente 40
+		
+	}
+	
 }
 
