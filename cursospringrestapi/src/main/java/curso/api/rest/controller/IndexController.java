b6b362bf5c1f85d39bfcddefd34044a1fc402679
AndreyBassani2004 <83016044+AndreyBassani2004@users.noreply.github.com>
2023-07-07 10:28:34 -0300

    Implementando o cache para performance.

--- cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursospringrestapi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -43,6 +44,7 @@ public class IndexController {
 	
 	//Serviço RESTFul
 	@GetMapping(value = "/{id}", produces = "application/json")
+	@Cacheable("cacheuser")
 	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
 		
 	Optional<Usuario> usuarios = usuarioRepository.findById(id);
@@ -88,11 +90,14 @@ public class IndexController {
 		return "OK";
 	}
 	
-	
+	/*Vamos supor que o carregamento de usuario seja um processo lento e queremos controlar seu cache*/
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<List<Usuario>> usuario (){
+	@Cacheable("cacheUsuarios")
+	public ResponseEntity<List<Usuario>> usuario () throws InterruptedException{
 		
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		Thread.sleep(6000);/*Segura o codigo em 6 segundos*/
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 		
